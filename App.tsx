93223d7a20b58a01d470619e4bb40a6bcbc4ed07
LucasC-Pinheiro <pinheirotj69@gmail.com>
2025-07-25 18:09:39 -0300

    Pasta Nativa

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { OneSignal } from 'react-native-onesignal'
+
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("3b09b1e8-6c64-4c87-a10a-7132f33728a3")
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
